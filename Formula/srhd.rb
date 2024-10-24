26a02ec97c788786916378f14efd1ba33ae2b32b
SylvanFranklin <sylvanfranklin@icloud.com>
2024-10-24 11:19:14 -0400

    functioning first version

--- Formula/srhd.rb
+++ Formula/srhd.rb
@@ -1,8 +1,8 @@
 class Srhd < Formula
   desc "Simple Rust Hotkey Daemon for MacOS"
   homepage "https://github.com/sylvanfranklin/srhd"
-  url "https://github.com/SylvanFranklin/srhd/releases/download/v0.2.0/srhd-mac.tar.gz"
-  sha256 "db43fcfe4515833f9a1c0e19b3028f567aa1348d25b3aaff64d154861c563082"
+  url "https://github.com/SylvanFranklin/srhd/releases/download/v0.3.0/srhd-mac.tar.gz"
+  sha256 "8a61b0da0513dd8b52e1a427ecc607052303adf0e5e716fd0cc13adcafc69d0b"
   version "0.2.0"
 
   def install
--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # SRHD formula
 
 Homebrew formula for building [srhd](https://github.com/SylvanFranklin/srhd) on macOS.
+
+```sh 
+brew tap sylvanfranklin/srhd 
+brew install srhd
+srhd start
+```

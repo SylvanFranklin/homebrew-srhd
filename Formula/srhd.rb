1ca79b53e31b1ca23f4236b6cdad95586a3742fd
SylvanFranklin <sylvanfranklin@icloud.com>
2024-10-24 11:25:25 -0400

    fixed version out of date

--- Formula/srhd.rb
+++ Formula/srhd.rb
@@ -3,7 +3,7 @@ class Srhd < Formula
   homepage "https://github.com/sylvanfranklin/srhd"
   url "https://github.com/SylvanFranklin/srhd/releases/download/v0.3.0/srhd-mac.tar.gz"
   sha256 "8a61b0da0513dd8b52e1a427ecc607052303adf0e5e716fd0cc13adcafc69d0b"
-  version "0.2.0"
+  version "0.3.0"
 
   def install
     bin.install "srhd"

61a4efa2b058e3d554b93f488f1bb7c691a3aeef
SylvanFranklin <sylvanfranklin@icloud.com>
2024-11-29 09:27:14 -0500

    updated to 0.4.0

--- Formula/srhd.rb
+++ Formula/srhd.rb
@@ -2,8 +2,8 @@ class Srhd < Formula
   desc "Simple Rust Hotkey Daemon for MacOS"
   homepage "https://github.com/sylvanfranklin/srhd"
   url "https://github.com/SylvanFranklin/srhd/releases/download/v0.3.1/srhd-mac.tar.gz"
-  sha256 "67f8b602b042dc927040721b4a81c52c63ccd6add74d8869f707c8640aa01522"
-  version "0.3.1"
+  sha256 "71095cd23ace476a9f84ffd90c7dc9b1c0ed1b74478d4f5aa8fe6c227a9bc3cd"
+  version "0.4.0"
 
   def install
     bin.install "srhd"
